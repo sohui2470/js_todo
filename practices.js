8421c6c40dec856a4a1f1b411ba1df95ba320881
sohui2470 <sohui2470@gmail.com>
2021-07-12 03:50:26 +0900

    todo service clone

---
+++ practices.js
@@ -0,0 +1,75 @@
+// Practices: Elements & Events
+
+const select = document.querySelector(".hello h1:first-child");
+console.log(select);
+
+function handleTitleClick(){
+    console.log("title was clicked!");
+    select.style.color = 'green';
+    alert("thanks a lot *'-'*");
+}
+function handleMouseEnter(){
+    console.log("mouse has entered!");
+    select.style.color = 'yellow';
+    select.innerText = "hello!"
+}
+function handleMouseLeave(){
+    console.log("mouse has left!");
+    select.style.color = 'black';
+    select.innerText = "T-T"
+}
+function handleResizeEvent(){
+    document.body.style.backgroundColor = 'lightgreen';
+}
+function handleCopyEvent(){
+    alert('well copied');
+}
+function handleOffEvent(){
+    alert('HEY!!!!!! you\'re offline now!!!');
+}
+function handleOnEvent(){
+    alert('YEY! you\'re online now!');
+}
+
+select.addEventListener("click", handleTitleClick);
+select.addEventListener("mouseenter", handleMouseEnter);
+// select.onmouseenter = handleMouseEnter;
+select.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleResizeEvent);
+window.addEventListener("copy", handleCopyEvent);
+window.addEventListener("offline", handleOffEvent);
+window.addEventListener("online", handleOnEvent);
+
+// Practices: About Color; understanding CSS
+
+const select = document.querySelector(".hello");
+document.body.style.backgroundColor = 'DarkSlateBlue';
+select.style.color = 'deepskyblue';
+
+function theNewStyle(){
+  const currentColor = select.style.color;
+  if(currentColor === 'deepskyblue'){
+    newColor = 'ghostwhite';
+  }else{
+    newColor = 'deepskyblue';
+  }
+  select.style.color = newColor;
+}
+
+select.addEventListener('click',theNewStyle);
+
+// Practices: About input without using form
+const loginForm = document.querySelector('#login-form');
+const loginInput = loginForm.querySelector('input');
+const loginButton = document.querySelector("#login-form button");
+
+function handleLoginButtonClick(){
+  const username = loginInput.value;
+  if (username === ''){
+    alert('please write your name');
+  } else if (username.length > 15 ){
+    alert("your name is too long!");
+  }
+}
+loginButton.addEventListener('click',handleLoginButtonClick);
